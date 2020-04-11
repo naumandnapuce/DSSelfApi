3b4ed22c8b30c0e5f7df065b80df50ad65357d5c
naumandnapuce <nnapuce@helius-software.com>
2020-04-11 02:01:03 +0200

    Update DocuRequest.cs

--- WebApi/Models/DocuRequest.cs
+++ WebApi/Models/DocuRequest.cs
@@ -8,8 +8,8 @@ namespace NISA.DSSelfAPI.WebApi.Models
     public class DocuRequest
     {
         public string ProviderName; // = "eToken Base Cryptographic Provider";
-        public string PinCode; //= "Akshi@123";
-        public string timestampServer; //=http://134.0.36.2:8081/
+        public string PinCode; 
+        public string timestampServer; 
         public byte[] BasePdf;
         public byte[] SignImage;
         public DocuRequest()
